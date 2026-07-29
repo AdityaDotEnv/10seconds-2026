edbf23149679e143e289d53e752b6c0e8245e8b4
Aditya K <adityak.10102005@gmail.com>
2026-07-29 13:10:40 +0530

    more tech stuff

--- Technical 5/Employee.java
+++ Technical 5/Employee.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Employee {
     private String name;
     private int age;
     private double salary;
+
+    private static int count = 0;
+
+    Employee() {
+        count++;
+    }
 
     public void getData(String name, int age, double salary) {
         this.name = name;
@@ -15,17 +22,65 @@ public class Employee {
         System.out.println("Employee Salary: " + salary);
         System.out.println();
     }
+
+    // Bubble sort by salary (Descending)
+    public static void sortData(Employee[] emp) {
+        Employee temp;
+
+        for (int i = 0; i < emp.length - 1; i++) {
+            for (int j = 0; j < emp.length - i - 1; j++) {
+                if (emp[j].salary < emp[j + 1].salary) {
+                    temp = emp[j];
+                    emp[j] = emp[j + 1];
+                    emp[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static int getCount() {
+        return count;
+    }
 }
 
 class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee();
-        e1.getData("Alex", 21, 32000);
-        e1.printData();
+        Scanner sc = new Scanner(System.in);
 
-        Employee e2 = new Employee();
-        e2.getData("Brian", 25, 15000);
-        e2.printData();
+        // Number of employees
+        System.out.print("Enter number of employees: ");
+        int n = sc.nextInt();
 
+        Employee[] emp = new Employee[n];
+
+        // Input employee details
+        for (int i = 0; i < n; i++) {
+            emp[i] = new Employee();
+
+            System.out.println("\nEnter details of Employee " + (i + 1));
+
+            System.out.print("Name: ");
+            String name = sc.next();
+
+            System.out.print("Age: ");
+            int age = sc.nextInt();
+
+            System.out.print("Salary: ");
+            double salary = sc.nextDouble();
+
+            emp[i].getData(name, age, salary);
+        }
+
+        Employee.sortData(emp);
+
+        System.out.println("\nEmployees Sorted by Salary (Highest to Lowest):\n");
+
+        for (int i = 0; i < n; i++) {
+            emp[i].printData();
+        }
+
+        System.out.println("Number of objects created in the program: " + Employee.getCount());
+
+        sc.close();
     }
 }
